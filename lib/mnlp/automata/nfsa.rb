032f8395fb641fb5e5042f0f850d1320b237f9b8
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2013-11-25 02:53:55 +0200

    Init Non Deterministic state machine

---
+++ lib/mnlp/automata/nfsa.rb
@@ -0,0 +1,85 @@
+# coding: utf-8
+
+module Mnlp
+  module Automata
+    # Non-Deterministic Finite State Automaton
+    class Nfsa
+      attr_reader :states, :current_state
+
+      # @param options [Hash]
+      # @option options [Fixnum] :number_of_states The initial number of states.
+      #   Additions (beyond initial number) can be made through {#add_state}
+      def initialize(options = {})
+        options = defaults.merge(options)
+        @states      = []
+        options[:number_of_states].times { add_state }
+        @current_state = states.first
+      end
+
+      # Adds a new {State}. At the moment there is no way to delete states from
+      # the machine.
+      def add_state
+        @states << State.new(id: states.size, deterministic: false)
+      end
+
+      # Gets machine alphabet from each state's alphabet
+      # @return [Set] the alphabet recognized by the machine
+      def alphabet
+        states.map(&:alphabet).reduce(Set.new) { |a, v| a += v }
+      end
+
+      def transitions
+        states.map(&:transitions).flatten
+      end
+
+      # Delegates transition creation to {Automata::State} class
+      #
+      # @param from [String, Fixnum] the name or id of from state
+      # @param to [String, Fixnum] the name or id of to state
+      # @param symbol [String] the symbol of transition
+      # @return [Array] from state's transitions
+      def create_transition(from, to, symbol)
+        raise Automata::Exceptions::NoStateError if !has_state?(from) || !has_state?(to)
+        from_state = find_state(from)
+        to_state   = find_state(to)
+
+        from_state.create_transition(to_state, symbol)
+      end
+
+      # @see #find_state
+      # @return [Boolean]
+      def has_state?(name_or_id)
+        find_state(name_or_id).present?
+      end
+
+      # Finds a state by its name or id
+      #
+      # @param name_or_id [String, Fixnum] the name or id of the state
+      # @return [Automata::State] or nil
+      def find_state(name_or_id)
+        states.select { |s| s.name == name_or_id || s.id == name_or_id }.first
+      end
+
+      def state_transition_table
+        states.reduce({}) do |result, state|
+          result[state.id] = state.transition_table
+          result
+        end
+      end
+
+      # @todo implement this
+      def recognize!(symbol)
+      end
+
+      private
+
+      def set_or_restore_current_state(symbol)
+      end
+
+      def defaults
+        { number_of_states: 1 }
+      end
+
+    end
+  end
+end
